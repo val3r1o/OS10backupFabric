8bea0b5e48e7c517bd9d9344d58efb264e73a379
Valerio Martini <valerio.martini@gmail.com>
2021-10-29 09:37:43 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -6,6 +6,7 @@ echo ------------------------------------------------- Dell NETWORKING ---------
 echo ------------------------------------------------------------------------------------------------------
 
 cd OS10backupFabric/
+#change the name as your project needs, check EVPN multisite or EVPN DCI for further examples 
 ansible-playbook -i inventory copyconfig.yml
 git status
 git pull
@@ -17,7 +18,7 @@ git status
 
 echo ----------- This is an automatic backup -------------------------------------------------------------
 echo - update                                                                                          ---
-echo ---- please note on git auth changes August 2021                                                  ---
+echo - please note on git auth changes August 2021                                                  ---
 echo - single password not accepted anymore                                                            ---
 echo - setup your token                                                                                ---
 echo - https://docs.github.com/en/github/authenticating-to-github/keeping-your-account-and-data-secure/creating-a-personal-access-token
