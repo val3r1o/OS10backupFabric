d8ac842b69c2b6599c00e5c11dadddcc2c1f9504
Valerio Martini <valerio.martini@gmail.com>
2021-11-18 17:08:43 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@ echo ------------------ This is an automatic backup ----------------------------
 echo ------------------------------------------------------------------------------------------------------
 echo ------------------------------------------------- Dell NETWORKING ------------------------------------
 echo ------------------------------------------------------------------------------------------------------
--
+
 # a git account and git running on the node is needed. https://docs.github.com/en 
 cd OS10backupFabric/
 #change the name as your project needs, check EVPN multisite or EVPN DCI for further examples 
@@ -16,30 +16,11 @@ git add .
 git commit -a -m "standard update"
 git push origin main
 git status
-
-
 echo ------------------------------------------------------------------------------------------------------
 echo ------------------ This is an automatic backup -------------------------------------------------------
 echo ------------------------------------------------------------------------------------------------------
 echo ------------------------------------------------- Dell NETWORKING ------------------------------------
-echo ------------------------------------------------------------------------------------------------------
--
--
-cd EVPN-multisite-superspine/
-ansible-playbook -i inventory copyconfig.yml
-git status
-git pull
-git pull
-git add .
-git commit -a -m "standard update"
-git push origin main
-git status
--
--
-echo ----------- This is an automatic backup -------------------------------------------------------------
-echo ------------------------------------------------------------------------------------------------------
-echo ---------------------------------------------- Dell NETWORKING --------------------------------------
-echo --------------------------valerio.martini@gmail.com--------------------------------------------------
+echo --------------------------valerio.martini@gmail.com---------------------------------------------------
 echo ------------------------------------------------------------------------------------------------------
 
 echo ---- please note on git auth changes August 2021 - single password not accepted anymore
